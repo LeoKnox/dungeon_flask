6627474e9b26e7a8d266509e9cdd88e8b6140458
LeoKnox <LeoKnox@users.noreply.github.com>
2020-03-25 23:05:42 -0700

    Add files via upload

--- application/routes_dungeon.py
+++ application/routes_dungeon.py
@@ -1,12 +1,14 @@
 from application import app
 from flask import render_template
 
-room = {"room_name":"Entry", "room_floor":"stone1","length":5,"width":5}
+room_list = [
+        {"room_name":"Entry", "room_floor":"stone1","length":5,"width":5},
+        {"room_name":"Throne", "room_floor":"Royal","length":6,"width":9}
+]
 
 @app.route('/rooms')
 def rooms():
-    print (room)
-    return render_template("rooms.html", rooms="active")
+    return render_template("rooms.html", rooms="active", room_list=room_list)
 
 @app.route('/levels')
 def levels():
